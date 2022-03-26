f0c5b30bd9670527ff4c7b42fe5937c10eeeed0a
MaxDuran08 <2997082920101@ingenieria.usac.edu.gt>
2022-03-26 16:53:15 -0600

    [v5]

--- Clase 2/Grafica de barras.py
+++ Clase 2/Grafica de barras.py
@@ -2,8 +2,17 @@ import matplotlib.pyplot as plt
 
 Etiquetas=['Enero','Febrero','Marzo','Abril']
 Valores=[4,5,3,2]
-Colores=['green','orange','red','purple']
 
+Archivo=open("./Clase 2/info.txt","r+")
+info=Archivo.read()
+print(info)
+info=info.split("\n")
+print(info)
+Etiquetas=info[0].split(",")
+Valores=[]
+for x in info[1].split(","):
+    Valores.append(int(x))
+Color=str(info[2])
 """
 Creando una grafica sencilla de barras, usamos el metodo bar
 seguido de sus etiquetas y sus valores
@@ -11,7 +20,7 @@ seguido de sus etiquetas y sus valores
 
 figura1=plt.figure(1)
 
-plt.bar(Etiquetas,Valores)
+plt.bar(Etiquetas,height=Valores)
 
 #plt.show
 
@@ -19,7 +28,7 @@ plt.bar(Etiquetas,Valores)
 Podemos agregarle sus titulos
 """
 figura2=plt.figure(2)
-plt.bar(Etiquetas,Valores)
+plt.bar(Etiquetas,height=Valores)
 
 plt.xlabel('Etiquetas')
 plt.ylabel('Valores')
@@ -32,14 +41,38 @@ Podemos cambiar el color de las barras con el parametro color
 """
 figura3=plt.figure(3)
 
-plt.bar(Etiquetas,Valores,color='red')
+plt.bar(Etiquetas,height=Valores,color='red')
+plt.xlabel('Etiquetas')
+plt.ylabel('Valores')
+plt.title('Grafica de barras')
+#plt.show()
+
+
+"""
+Colores para cada uno
+"""
+
+figura4=plt.figure(4)
+
+Colores=["red","green","blue","purple"]
+
+plt.bar(Etiquetas,height=Valores,color=Colores)
+plt.xlabel('Etiquetas')
+plt.ylabel('Valores')
+plt.title('Grafica de barras')
+#plt.show()
+
+
+"""
+ancho de la grafica
+"""
+figura5=plt.figure(5)
+
+plt.bar(Etiquetas,height=Valores,color=Colores,width=0.5)
 plt.xlabel('Etiquetas')
 plt.ylabel('Valores')
 plt.title('Grafica de barras')
 plt.show()
-
-
-
 
 
 
